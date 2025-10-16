2b6ad5383b6d8e19a754bafcd7ecfb197b5344d4
thidantas <eng.thiagohpdantas@gmail.com>
2025-10-16 11:47:27 -0300

    feat(screens): create Login and SignUp screens and adjust main app entry to display them

--- App.tsx
+++ App.tsx
@@ -1,48 +1,17 @@
-import { View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ThemeProvider } from '@shopify/restyle'
 
-import { Text } from './src/components/Text/Text'
-import { Button } from './src/components/Button/Button'
 import { theme } from './src/theme/theme'
-import { Icon } from './src/components/Icon/Icon'
-import { TextInput } from './src/components/TextInput/TextInput'
+import { SignUpScreen } from './src/screens/auth/SignUpScreen/SignUpScreen'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{ paddingHorizontal: 24 }}>
-          <Text preset="headingLarge" marginBottom="s8">
-            Olá
-          </Text>
-          <Text preset="paragraphLarge" mb="s40">
-            Digite seu e-mail e senha para entrar
-          </Text>
-
-          <TextInput
-            errorMessage="mensagem de erro"
-            label="E-mail"
-            placeholder="Digite seu e-mail"
-            boxProps={{ mb: 's10' }}
-          />
-
-          <TextInput
-            label="Senha"
-            placeholder="Digite sua senha"
-            RightComponent={<Icon name="eyeOn" color="gray2" />}
-            boxProps={{ mb: 's20' }}
-          />
-
-          <Text color="primary" preset="paragraphSmall" bold mt="s10">
-            Esqueci minha senha
-          </Text>
-
-          <Button title="Entrar" marginTop="s48" />
-          <Button preset="outline" title="Criar uma conta" mt="s12" />
-        </View>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        {/* <LoginScreen /> */}
+        <SignUpScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   )
 }
 
---
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,45 @@
+import { Button } from '../../../components/Button/Button'
+import { PasswordInput } from '../../../components/PasswordInput/PasswordInput'
+import { Screen } from '../../../components/Screen/Screen'
+import { Text } from '../../../components/Text/Text'
+import { TextInput } from '../../../components/TextInput/TextInput'
+
+export function SignUpScreen() {
+  function submitForm() {
+    //TODO: implementation of form submission logic
+  }
+
+  return (
+    <Screen canGoBack scrollable>
+      <Text preset="headingLarge" mb="s32">
+        Criar uma conta
+      </Text>
+
+      <TextInput
+        label="Seu username"
+        placeholder="@"
+        boxProps={{ mb: 's20' }}
+      />
+
+      <TextInput
+        label="Nome Completo"
+        placeholder="Digite seu nome completo"
+        boxProps={{ mb: 's20' }}
+      />
+
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{ mb: 's20' }}
+      />
+
+      <PasswordInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        boxProps={{ mb: 's48' }}
+      />
+
+      <Button onPress={submitForm} title="Criar uma conta" />
+    </Screen>
+  )
+}
