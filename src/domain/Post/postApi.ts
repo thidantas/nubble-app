85bca936b2be3a360fca11b4e1f000ff25dfde25
thidantas <eng.thiagohpdantas@gmail.com>
2025-10-26 15:45:39 -0300

    feat(domain): create initial structure for Post domain with mocked service

---
+++ src/domain/Post/postApi.ts
@@ -0,0 +1,11 @@
+import { postListMock } from './postListMock'
+import { Post } from './types'
+
+async function getList(): Promise<Post[]> {
+  await new Promise(resolve => setTimeout(() => resolve(''), 1000))
+  return postListMock
+}
+
+export const postApi = {
+  getList
+}
