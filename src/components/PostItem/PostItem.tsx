b3914a1129ce9a43270e5ec51e5c9a774c6a8a48
thidantas <eng.thiagohpdantas@gmail.com>
2025-10-26 15:50:07 -0300

    feat(post-item): create PostItem component with modular subcomponents

---
+++ src/components/PostItem/PostItem.tsx
@@ -0,0 +1,31 @@
+import { Post } from '@domain'
+
+import { Box } from '../Box/Box'
+
+import { PostActions } from './components/PostActions'
+import { PostBottom } from './components/PostBottom'
+import { PostHeader } from './components/PostHeader'
+import { PostImage } from './components/PostImage'
+
+interface PostItemProps {
+  post: Post
+}
+
+export function PostItem({ post }: PostItemProps) {
+  return (
+    <Box marginBottom="s24" paddingHorizontal="s24">
+      <PostHeader author={post.author} />
+      <PostImage imageURL={post.imageURL} />
+      <PostActions
+        commentCount={post.commentCount}
+        favoriteCount={post.favoriteCount}
+        reactionCount={post.reactionCount}
+      />
+      <PostBottom
+        author={post.author}
+        text={post.text}
+        commentCount={post.commentCount}
+      />
+    </Box>
+  )
+}
