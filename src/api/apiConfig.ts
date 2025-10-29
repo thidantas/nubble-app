23e8a97a437c7085fcabc17271efe70ea7f4ca56
thidantas <eng.thiagohpdantas@gmail.com>
2025-10-29 17:20:51 -0300

    chore(api): add TODO in apiConfig

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -3,7 +3,8 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333',
   headers: {
+    // TODO: temporary token in use, replace with securely stored token and apply masking
     Authorization:
-      'Bearer eyJhbGciOiJSUzI1NiJ9.eyJkYXRhIjp7InVzZXJJZCI6MSwidXNlciI6eyJlbWFpbCI6Im1hcmlhanVsaWFAY29mZnN0YWNrLmNvbSJ9fSwiaWF0IjoxNzYxNzY1MjI1LCJleHAiOjE3NjE3NjcwMjV9.D0rJ09l4kAGLLTn7dk9hMZewYOPCkMp4eekxzXHPpS9SPqyP835Av6aAYuuAz7jEXnZXdC04x5N5w_Wp13-_ed4hbHd_fs6Oo9UnabP-NGnytQD1S6fTYg10lkUHtS8Hv3IG1du-l1xuwV20XlQZr7vO7wcdXNU4e25E_Pa1MzF--ZIadiwbBDrRoiz83XmWiOzH9tAoXZu9kbtWmEgsUfjOUkp3AS3d_yFzHyotrkxtjL3-ab3BeF-0Fw2fhI-x3Z1cjAlmFSw38HcKoxn0NH7mAVynsolnpxXUtak0DpsqPpk1bKM100Pj0Fp1BhAGxcgZ2zxHp21I1mZ7tFOqZg'
+      'Bearer eyJhbGciOiJSUzI1NiJ9.eyJkYXRhIjp7InVzZXJJZCI6MSwidXNlciI6eyJlbWFpbCI6Im1hcmlhanVsaWFAY29mZnN0YWNrLmNvbSJ9fSwiaWF0IjoxNzYxNzY4NDg1LCJleHAiOjE3NjE3NzAyODV9.BOWXtgvSvVFpp_DsCqcPFVBvZu0o0WAfp-ymDFPsNAZUF1b_xZq9kMrvQXWYV4DuRoNqjCYTMosgB-NnCM_ugH2s24Yns0kkkpV-5rTD8oP6Nq4B8JrG0GArNn-9R8II7d0rtUteitWVtDbP-505Wo_cD5ant5Cm29W6hCrEbgXd8FJ6mRmChy3JvFj7Je0IapFeS6TnoNwFzXTfA4-swBc-iDMG6sPmvAiXgsh4At6x8WYKFFQ9I6r4AdBTOvT_uhSOGWQ-rEro6X5BhatRlEN0_Gu7-BvjadfMvVpioJ5sKS-Cj13_u7u5HTm8dQ_tevaLCFZThL4sEhfCJRlEUw'
   }
 })
