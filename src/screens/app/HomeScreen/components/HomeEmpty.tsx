c2b2363ab65c2d04331a2d94a8ea9f5b1ec7b82c
thidantas <eng.thiagohpdantas@gmail.com>
2025-10-29 16:59:58 -0300

    feat(home): create HomeEmpty component for HomeScreen

---
+++ src/screens/app/HomeScreen/components/HomeEmpty.tsx
@@ -0,0 +1,41 @@
+import { ActivityIndicator, Box, Button, Text } from '@components'
+
+interface HomeEmptyProps {
+  loading: boolean
+  error: unknown
+  refetch: () => void
+}
+
+export function HomeEmpty({ loading, error, refetch }: HomeEmptyProps) {
+  let component = (
+    <Text preset="paragraphMedium" bold>
+      Não há publicações no seu feed
+    </Text>
+  )
+
+  if (loading) {
+    component = <ActivityIndicator color="primary" />
+  }
+
+  if (error) {
+    component = (
+      <>
+        <Text preset="paragraphMedium" bold mb="s16">
+          Não foi possível carregar o feed 😢
+        </Text>
+        <Button title="recarregar" preset="outline" onPress={refetch} />
+      </>
+    )
+  }
+
+  return (
+    <Box
+      backgroundColor="carrotSecondary"
+      flex={1}
+      justifyContent="center"
+      alignItems="center"
+    >
+      {component}
+    </Box>
+  )
+}
