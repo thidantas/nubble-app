19bc406e6e10bfbcc840df13c1cdfdab9965634c
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-01 20:12:38 -0300

    feat(post-comment-adapter): create postCommentAdapter

---
+++ src/domain/PostComment/postCommentAdapter.ts
@@ -0,0 +1,25 @@
+/**
+ * @description Transforms a PostCommentAPI object into a PostComment object
+ * @param postCommentAPI
+ * @returns PostComment
+ */
+
+import { PostComment, PostCommentAPI } from './postCommentTypes'
+
+function toPostComment(postCommentAPI: PostCommentAPI): PostComment {
+  return {
+    id: postCommentAPI.id,
+    message: postCommentAPI.message,
+    createdAt: postCommentAPI.created_at,
+    author: {
+      id: postCommentAPI.user.id,
+      name: postCommentAPI.user.full_name,
+      profileURL: postCommentAPI.user.profile_url,
+      userName: postCommentAPI.user.username
+    }
+  }
+}
+
+export const postCommentAdapter = {
+  toPostComment
+}
