864b2d40eac9687f676e4091b966aa892f1c3e45
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-04 16:40:08 -0300

    feat(domain-post-comment): create usePostComment

---
+++ src/domain/PostComment/useCases/usePostCommentCreate.ts
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+import { postCommentService } from '../postCommentService'
+import { PostComment } from '../postCommentTypes'
+
+interface Options {
+  onSuccess?: (data: PostComment) => void
+  onError?: (message: string) => void
+}
+
+export function usePostCommentCreate(postId: number, options?: Options) {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<boolean | null>(null)
+
+  async function createComment(message: string) {
+    try {
+      setLoading(true)
+      setError(null)
+      const postComment = await postCommentService.create(postId, message)
+
+      if (options?.onSuccess) {
+        options.onSuccess(postComment)
+      }
+    } catch (err) {
+      if (options?.onError) {
+        options.onError('Erro ao criar comentário')
+      }
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {
+    createComment,
+    loading,
+    error
+  }
+}
