81e8c90e172321ba0879851bb00c26ef3052572e
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-04 18:42:18 -0300

    feat(post-comment-usecase): create usePostCommentRemove

--- src/domain/PostComment/index.ts
+++ src/domain/PostComment/index.ts
@@ -1,5 +1,4 @@
 export * from './postCommentTypes'
 export * from './postCommentService'
 
-export * from './useCases/usePostCommentList'
-export * from './useCases/usePostCommentCreate'
+export * from './useCases'
