9de0d6a0f3dfda80ccd8d5d73627a03e04aacfcb
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-06 17:01:23 -0300

    feat(app): add ToastProvider to App layer to enable usage of alternative toast context

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
+// import { ToastProvider } from '@services'
 import { ThemeProvider } from '@shopify/restyle'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+import { Toast } from '@components'
 import { Router } from '@routes'
 import { theme } from '@theme'
 
@@ -8,7 +10,11 @@ function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
+        {/* The `ToastProvider` should only be used for the layer with the implemented context. */}
+        {/* <ToastProvider> */}
         <Router />
+        <Toast />
+        {/* </ToastProvider>  */}
       </ThemeProvider>
     </SafeAreaProvider>
   )
