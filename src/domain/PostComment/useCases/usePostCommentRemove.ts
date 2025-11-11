aba493dc87774a889784b3c64e8148c637a8124b
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-11 18:07:49 -0300

    refactor(post-comment): update usePostCommentRemove useCase to use TanStack useMutation

--- src/domain/PostComment/useCases/usePostCommentRemove.ts
+++ src/domain/PostComment/useCases/usePostCommentRemove.ts
@@ -1,10 +1,34 @@
-import { MutationOptions, useMutation } from '@infra'
+import { MutationOptions, QueryKeys } from '@infra'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { postCommentService } from '../postCommentService'
 
-export function usePostCommentRemove(option?: MutationOptions<string>) {
-  return useMutation<{ postCommentId: number }, string>(
-    ({ postCommentId }) => postCommentService.remove(postCommentId),
-    option
-  )
+export function usePostCommentRemove(
+  postId: number,
+  option?: MutationOptions<string>
+) {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<string, unknown, { postCommentId: number }>({
+    mutationFn: ({ postCommentId }) => postCommentService.remove(postCommentId),
+    onSuccess: message => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.PostCommentList, postId]
+      })
+      if (option?.onSuccess) {
+        option.onSuccess(message)
+      }
+    },
+    onError: () => {
+      if (option?.onError) {
+        option.onError(
+          option?.errorMessage || 'ocorreu um erro na exclusão do comentário'
+        )
+      }
+    }
+  })
+
+  return {
+    mutate: mutation.mutate
+  }
 }
