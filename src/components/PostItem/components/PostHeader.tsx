4e9d964ceb31e08b2f08db9158dbdad7f12de635
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-11 17:34:29 -0300

    feat(post-header): add navigation to ProfileScreen from PostHeader component

--- src/components/PostItem/components/PostHeader.tsx
+++ src/components/PostItem/components/PostHeader.tsx
@@ -1,17 +1,28 @@
+import { Pressable } from 'react-native'
+
 import { Post } from '@domain'
+import { useNavigation } from '@react-navigation/native'
 
 import { Box, ProfileAvatar, Text } from '@components'
 
 type PostHeaderProps = Pick<Post, 'author'>
 
 export function PostHeader({ author }: PostHeaderProps) {
-  return (
-    <Box flexDirection="row" alignItems="center" mb="s16">
-      <ProfileAvatar imageURL={author.profileURL} />
+  const navigation = useNavigation()
 
-      <Text preset="paragraphMedium" ml="s12" semiBold>
-        {author.userName}
-      </Text>
-    </Box>
+  function navigateToProfile() {
+    navigation.navigate('ProfileScreen', { userId: author.id })
+  }
+
+  return (
+    <Pressable onPress={navigateToProfile}>
+      <Box flexDirection="row" alignItems="center" mb="s16">
+        <ProfileAvatar imageURL={author.profileURL} />
+
+        <Text preset="paragraphMedium" ml="s12" semiBold>
+          {author.userName}
+        </Text>
+      </Box>
+    </Pressable>
   )
 }
