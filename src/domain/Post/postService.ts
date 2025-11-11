f8ee19fb78288d46589f1aaea0d27901614dbaa4
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-11 18:20:38 -0300

    chore(domain-post): remove commented code from postService

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -6,10 +6,8 @@ import { postApi } from './postApi'
 import { Post } from './postTypes'
 
 async function getList(page: number): Promise<Page<Post>> {
-  const postPageAPI = await postApi.getList({ page, per_page: 5 })
+  const postPageAPI = await postApi.getList({ page, per_page: 10 })
 
-  // throw new Error('Not implemented')
-  // return []
   return {
     data: postPageAPI.data.map(postAdapter.toPost),
     meta: apiAdapter.toMetaDataPage(postPageAPI.meta)
