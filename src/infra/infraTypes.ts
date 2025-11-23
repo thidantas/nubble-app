115ca08997b5cddea0968fd2a7e96650d66d47ea
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-23 13:24:07 -0300

    chore(domain/auth): add new queryKeys for auth useCases to support TanStack integration

--- src/infra/infraTypes.ts
+++ src/infra/infraTypes.ts
@@ -1,5 +1,7 @@
 export enum QueryKeys {
   PostList = 'PostList',
   PostCommentList = 'PostCommentList',
-  UserGetById = 'UserGetById'
+  UserGetById = 'UserGetById',
+  IsUsernameAvailable = 'IsUsernameAvailable',
+  IsEmailAvailable = 'IsEmailAvailable'
 }
