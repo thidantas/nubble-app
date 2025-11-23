af7f3963fedc899f04f49e08eff7ec52235f0c7a
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-23 13:14:30 -0300

    feat(routes): add authentication service context to handle global auth flow

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { useAuthCredentials } from '@services'
+
+import { ActivityIndicator, Box } from '@components'
 
 import { AppStack } from './AppStack'
 import { AuthStack } from './AuthStack'
 
 export function Router() {
-  const authenticated = true
+  // const authenticated = false
+
+  const { authCredentials, isLoading } = useAuthCredentials()
+
+  if (isLoading) {
+    return (
+      <Box
+        flex={1}
+        backgroundColor="background"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <ActivityIndicator size="large" />
+      </Box>
+    )
+  }
 
   return (
     <NavigationContainer>
-      {authenticated ? <AppStack /> : <AuthStack />}
+      {authCredentials ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   )
 }
