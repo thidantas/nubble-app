48953d75006e7d53947cdda9f633a70af2ba27e3
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-29 20:35:53 -0300

    test(config): create test-utils with custom render for provider wrapper

---
+++ src/test/test-utils.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { ThemeProvider } from '@shopify/restyle'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { render, RenderOptions } from '@testing-library/react-native'
+
+import { theme } from '@theme'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity
+    },
+    mutations: {
+      retry: false,
+      cacheTime: Infinity
+    }
+  }
+})
+
+export const AllTheProviders = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>{children}</NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
+
+function customRender<T = unknown>(
+  component: ReactElement<T>,
+  options?: Omit<RenderOptions, 'wrapper'>
+) {
+  return render(component, { wrapper: AllTheProviders, ...options })
+}
+
+export * from '@testing-library/react-native'
+export { customRender as render }
