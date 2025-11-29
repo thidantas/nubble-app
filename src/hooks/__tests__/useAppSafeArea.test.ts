d5e8b77b0fcb3741998d5d79a8442c2d11bb9913
thidantas <eng.thiagohpdantas@gmail.com>
2025-11-29 20:46:13 -0300

    test(hooks): add unit tests for useAppSafeArea hook

---
+++ src/hooks/__tests__/useAppSafeArea.test.ts
@@ -0,0 +1,50 @@
+import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context'
+import { AllTheProviders, renderHook } from 'test-utils'
+
+import { theme } from '@theme'
+
+import { useAppSafeArea } from '../useAppSafeArea'
+
+jest.mock('react-native-safe-area-context')
+
+const mockedUsedSafeAreaInsets = jest.mocked(useSafeAreaInsets)
+
+describe('useSafeArea', () => {
+  describe('when the safe area is less than minimum requirement', () => {
+    mockedUsedSafeAreaInsets.mockImplementationOnce(
+      () =>
+        ({
+          top: 5,
+          bottom: 5
+        } as EdgeInsets)
+    )
+
+    it('return the minimum requirement', () => {
+      const { result } = renderHook(() => useAppSafeArea(), {
+        wrapper: AllTheProviders
+      })
+
+      expect(result.current.top).toBe(theme.spacing.s20)
+      expect(result.current.bottom).toBe(theme.spacing.s20)
+    })
+  })
+
+  describe('when the safe area is greater than minimum requirement', () => {
+    it('returns the safe area', () => {
+      mockedUsedSafeAreaInsets.mockImplementationOnce(
+        () =>
+          ({
+            top: 30,
+            bottom: 30
+          } as EdgeInsets)
+      )
+
+      const { result } = renderHook(() => useAppSafeArea(), {
+        wrapper: AllTheProviders
+      })
+
+      expect(result.current.top).toEqual(30)
+      expect(result.current.bottom).toEqual(30)
+    })
+  })
+})
