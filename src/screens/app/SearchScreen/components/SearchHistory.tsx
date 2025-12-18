af4acd15898d50e0a49bc732e7c287f4f2ed641c
thidantas <eng.thiagohpdantas@gmail.com>
2025-12-18 11:33:20 -0300

    feat(search): implement SearchHistory component with removal logic

---
+++ src/screens/app/SearchScreen/components/SearchHistory.tsx
@@ -0,0 +1,43 @@
+import { FlatList, ListRenderItemInfo } from 'react-native'
+
+import { User } from '@domain'
+import { useSearchHistory, useSearchHistoryService } from '@services'
+
+import { Box, Icon, ProfileUser, Text } from '@components'
+
+export function SearchHistory() {
+  const userList = useSearchHistory()
+
+  const { removeUser } = useSearchHistoryService()
+
+  function renderItem({ item }: ListRenderItemInfo<User>) {
+    return (
+      <ProfileUser
+        user={item}
+        avatarProps={{ size: 48 }}
+        RightComponent={
+          <Icon
+            name="trash"
+            color="gray3"
+            size={14}
+            onPress={() => removeUser(item.id)}
+          />
+        }
+      />
+    )
+  }
+
+  return (
+    <Box>
+      <FlatList
+        ListHeaderComponent={
+          <Text mb="s16" preset="headingMedium">
+            Buscas recentes
+          </Text>
+        }
+        data={userList}
+        renderItem={renderItem}
+      />
+    </Box>
+  )
+}
