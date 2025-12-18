b1c54914885df488f6beb766bc96cdfcf67bf9d8
thidantas <eng.thiagohpdantas@gmail.com>
2025-12-18 11:20:20 -0300

    feat(ui): enhance ProfileUser with avatar customization and interactivity

--- src/components/ProfileUser/ProfileUser.tsx
+++ src/components/ProfileUser/ProfileUser.tsx
@@ -1,28 +1,56 @@
-import { Pressable } from 'react-native'
+import { GestureResponderEvent } from 'react-native'
 
 import { User } from '@domain'
 import { useNavigation } from '@react-navigation/native'
 
-import { Box, ProfileAvatar, Text } from '@components'
+import {
+  PressableBox,
+  ProfileAvatar,
+  Text,
+  PressableBoxProps,
+  ProfileAvatarProps,
+  Box
+} from '@components'
 
-type ProfileUserProps = { user: Pick<User, 'username' | 'profileUrl' | 'id'> }
+type ProfileUserProps = {
+  user: Pick<User, 'username' | 'profileUrl' | 'id'>
+  avatarProps?: Omit<Partial<ProfileAvatarProps>, 'imageURL'>
+  RightComponent?: React.ReactElement
+} & PressableBoxProps
 
-export function ProfileUser({ user }: ProfileUserProps) {
+export function ProfileUser({
+  user,
+  avatarProps,
+  RightComponent,
+  onPress,
+  ...pressableBoxProps
+}: ProfileUserProps) {
   const navigation = useNavigation()
 
-  function navigateToProfile() {
+  function handleOnPress(event: GestureResponderEvent) {
+    if (onPress) {
+      onPress(event)
+    }
+
     navigation.navigate('ProfileScreen', { userId: user.id })
   }
 
   return (
-    <Pressable onPress={navigateToProfile}>
-      <Box flexDirection="row" alignItems="center" mb="s16">
-        <ProfileAvatar imageURL={user.profileUrl} />
-
+    <PressableBox
+      flexDirection="row"
+      alignItems="center"
+      mb="s16"
+      justifyContent="space-between"
+      onPress={handleOnPress}
+      {...pressableBoxProps}
+    >
+      <Box flexDirection="row" alignItems="center" justifyContent="center">
+        <ProfileAvatar {...avatarProps} imageURL={user.profileUrl} />
         <Text preset="paragraphMedium" ml="s12" semiBold>
           {user.username}
         </Text>
       </Box>
-    </Pressable>
+      {RightComponent}
+    </PressableBox>
   )
 }
