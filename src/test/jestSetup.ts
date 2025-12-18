9f0d9724da174819ae7db0c05ce3450fcc632ffd
thidantas <eng.thiagohpdantas@gmail.com>
2025-12-18 11:54:35 -0300

    test(storage): implement in-memory storage mock for Jest

--- src/test/jestSetup.ts
+++ src/test/jestSetup.ts
@@ -1,5 +1,8 @@
 import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock'
 
+import { inMemoryStorage } from '../services/storage/implementation/jest/inMemoryStorage'
+import { initializeStorage } from '../services/storage/storage'
+
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext)
 
 jest.mock('@react-navigation/native', () => {
@@ -12,3 +15,5 @@ jest.mock('@react-navigation/native', () => {
     })
   }
 })
+
+initializeStorage(inMemoryStorage)
