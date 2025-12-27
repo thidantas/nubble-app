76ed0575449fa8259927ada1c285de5d35a8be62
thidantas <eng.thiagohpdantas@gmail.com>
2025-12-27 20:27:31 -0300

    feat(service): implement getPhotos service for gallery listing

---
+++ src/services/cameraRoll/cameraRollService.ts
@@ -0,0 +1,23 @@
+import { CameraRoll } from '@react-native-camera-roll/camera-roll'
+
+import { PhotoListPaginated } from './cameraRollTypes'
+
+async function getPhotos(cursor?: string): Promise<PhotoListPaginated> {
+  console.log('get photos')
+
+  const photoPage = await CameraRoll.getPhotos({ first: 12, after: cursor })
+
+  const photoList = photoPage.edges.map(edge => edge.node.image.uri)
+
+  console.log('cursor', cursor)
+  console.log('hasNextPage:', photoPage.page_info.has_next_page)
+  return {
+    photoList,
+    cursor: photoPage.page_info.end_cursor,
+    hasNextPage: photoPage.page_info.has_next_page
+  }
+}
+
+export const cameraRollService = {
+  getPhotos
+}
