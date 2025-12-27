3ac5c1e5655343dcd2e49e416267dc4e3825c74e
thidantas <eng.thiagohpdantas@gmail.com>
2025-12-27 20:18:41 -0300

    feat(permission): implement platform-specific permission services for Android and iOS

---
+++ src/services/permission/permissionService.ios.ts
@@ -0,0 +1,44 @@
+import {
+  request as rnpRequest,
+  check as rnpCheck,
+  PermissionStatus as RnpPermissionStatus,
+  PERMISSIONS as RNP_PERMISSIONS,
+  Permission as RnpPermission
+} from 'react-native-permissions'
+
+import { PermissionName, PermissionStatus } from './permissionTypes'
+
+const mapName: Record<PermissionName, RnpPermission> = {
+  photoLibrary: RNP_PERMISSIONS.IOS.PHOTO_LIBRARY,
+  camera: RNP_PERMISSIONS.IOS.CAMERA
+}
+
+const mapStatus: Record<RnpPermissionStatus, PermissionStatus> = {
+  blocked: 'never_ask_again',
+  denied: 'denied',
+  granted: 'granted',
+  limited: 'granted',
+  unavailable: 'unavailable'
+}
+
+async function check(name: PermissionName): Promise<PermissionStatus> {
+  console.log('PERMISSION IOS')
+
+  const status = await rnpCheck(mapName[name])
+
+  console.log('status ios', status)
+  return mapStatus[status]
+}
+
+async function request(name: PermissionName): Promise<PermissionStatus> {
+  console.log('PERMISSION IOS')
+
+  const status = await rnpRequest(mapName[name])
+
+  return mapStatus[status]
+}
+
+export const permissionService = {
+  request,
+  check
+}
