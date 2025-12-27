5dd264c2d2e01d99fdc79b8e65d6bc118ea58157
thidantas <eng.thiagohpdantas@gmail.com>
2025-12-27 20:33:01 -0300

    chore(assets): add images directory with placeholder and configure path aliases

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import { ImageSourcePropType } from 'react-native'
+
+export const images: { [key: string]: ImageSourcePropType } = {
+  imagePlaceholder: require('./images/image_placeholder.png')
+}
